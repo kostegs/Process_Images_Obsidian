dbc856d8243bcf9a83035612296a28e819e82aa2
Kostegs <ksuslin@gmail.com>
2024-08-04 21:23:45 +0300

    compress images

--- start.py
+++ start.py
@@ -1,13 +1,18 @@
 import re
 import shutil
+import os.path
+from PIL import Image
 
-file_path = './Test to WP.md'
+file_path = './Timeline.md'
 storage_path = 'E:/Obsidian_Storage/Storage'
+old_images_path = './img_old/'
+compressed_images_path = './img_new/'
+
 
 def find_all_image_names():
     file_contents = ''
     try:
-        with open(file_path, 'r', encoding = 'utf-8') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             file_contents = f.read()
     except Exception as e:
         print(f'Can\'t read the file, because: {e}')
@@ -25,10 +30,19 @@ def find_all_image_names():
 
     return finish_list
 
+
+def check_image_path():
+    if not os.path.exists(old_images_path):
+        os.makedirs(old_images_path)
+    if not os.path.exists(compressed_images_path):
+        os.makedirs(compressed_images_path)
+
+
 def copy_old_images(image_names):
     for image in image_names:
         source_path = f'{storage_path}/Images/{image["Name"]}'
-        dest_path = f'./img_old/{image["Name"]}'
+        dest_path = f'{old_images_path}{image["Name"]}'
+
         try :
             shutil.copy(source_path, dest_path)
         except FileNotFoundError:
@@ -36,8 +50,33 @@ def copy_old_images(image_names):
         except Exception:
             print(f'Error while copying image file: {Exception}')
 
+
+def compress_img(image_name, new_image_name, quality):
+    # load the image to memory
+    with Image.open(image_name, 'r') as img:
+        # change the extension to JPEG
+        new_filename = f"{new_image_name}.jpg"
+
+        try:
+            # save the image with the corresponding quality and optimize set to True
+            img.save(new_filename, quality=quality, optimize=True)
+        except OSError:
+            # convert the image to RGB mode first
+            img = img.convert("RGB")
+            # save the image with the corresponding quality and optimize set to True
+            img.save(new_filename, quality=quality, optimize=True)
+
+        print("[+] New file saved:", new_filename)
+
+
 if __name__ == '__main__':
     images = find_all_image_names()
+    check_image_path()
     copy_old_images(images)
+
+    for image in images:
+        source_image = f'{old_images_path}{image['Name']}'
+        dest_image = f'{compressed_images_path}{image['NewName']}'
+        compress_img(source_image, dest_image, 80)
 
 
